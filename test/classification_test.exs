e346b11d1296aa60e2d38be98e67f58a5224b0c0
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-03-25 16:43:03 -0400

    refactor

--- test/classification_test.exs
+++ test/classification_test.exs
@@ -10,26 +10,9 @@ defmodule Beans.ClassificationTest do
     {:ok, %{}}
   end
 
-  test "build_store/0", context do
-    Beans.Classification.build_store
-    pid = Process.whereis(Beans.Classification)
-    {:messages, messages} = Process.info(pid, :messages)
-
-    valid_messages = messages
-    |> Enum.filter(fn{k, v} ->
-      k == :"$gen_cast" && v |> elem(0) == :build_store
-    end)
-
-    assert valid_messages |> Enum.count > 0
-  end
-
   test "handler builds store", context do
     pid = Process.whereis(Beans.Classification)
-    GenServer.cast(pid, {:build_store, [@classification_file_path]})
-
-    #sleep for 25ms so the process handles the message
-    :timer.sleep(25)
-
+    
     process_store = :sys.get_state(pid) |> Map.get(:store, [])
     assert process_store |> Map.keys |> Enum.count > 0
   end
