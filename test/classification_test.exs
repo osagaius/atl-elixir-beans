82516990c5cdf80e3df727ff648f199ec58c7239
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-04-04 01:58:10 -0400

    refactor

--- test/classification_test.exs
+++ test/classification_test.exs
@@ -11,10 +11,7 @@ defmodule Beans.ClassificationTest do
   end
 
   test "store is built", context do
-    pid = Process.whereis(Beans.Classification)
-
-    process_store = :sys.get_state(pid) |> Map.get(:store, [])
-    assert process_store |> Map.keys |> Enum.count > 0
+    assert Beans.Classification.get_classification("pinto") == "phaseolus"
   end
 
 
