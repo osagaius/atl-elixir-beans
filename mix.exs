d385ae06b1734d4dea70e3ab98bf714da15d639a
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-04-04 01:57:42 -0400

    Add dep

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Beans.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Beans, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :amnesia]]
   end
 
   # Specifies which paths to compile per environment.
@@ -33,6 +33,8 @@ defmodule Beans.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:amnesia, "~> 0.2.0"},
+   ]
   end
 end
