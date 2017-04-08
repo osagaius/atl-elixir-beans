f178b74612cb56c16d91571d4f16abb5ed2c09a2
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-04-08 14:33:19 -0400

    use default port

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :beans, Beans.Endpoint,
-  http: [port: System.get_env("PORT")],
+  http: [port: System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
